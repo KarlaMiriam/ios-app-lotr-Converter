5d345b682e7de089e48334de4f7702167ce5feaf
Karla <karlamiriam6@gmail.com>
2024-10-08 00:09:25 -0300

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Karla Miriam Dos santos Goncalves on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
